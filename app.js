a60c8018b34ecb6a8740cd83904cc960203b906b
teachercca02 <sui423@gmail.com>
2024-12-01 04:14:04 +0900

    20241201

--- app.js
+++ app.js
@@ -46,4 +46,56 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+// cu add start
+var http = require('http');
+var port = normalizePort(process.env.PORT || '30121');
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port, '0.0.0.0', function () {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
+});
+
+server.on('error', onError);
+server.on('listening', onListening);
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
+//cu  add end
+
 module.exports = app;
